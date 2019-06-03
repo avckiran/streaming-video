6d448ddb333eda50e6fe57d60b63566ec0fd63d1
Chandra Kiran Achanta <avckiran>
2019-06-02 23:06:04 -0500

    CRUD functionality completed

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,74 +1,22 @@
 import React from 'react';
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
-import { createStream} from "../../actions"
+import { createStream} from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component{
-    renderError ({error, touched}){
-        if(touched && error){
-            return (
-                <div className="ui error message">
-                    <div className="header">{error}</div>
-                </div>
-            );
-        }
-    }
-    renderInput = ({input, label, meta}) => {
-        // return <input onChange={formProps.input.onChange} value={formProps.input.value} />;
-        // console.log(meta);
-        const className=`field ${meta.error && meta.touched ? 'error':''}`;
-        return ( 
-            <div className={className}>
-                <label>{label}</label>
-                <input {...input} autoComplete="off" />
-                {this.renderError(meta)}
-            </div>
-
-        );
-    }
 
     onSubmit = formValues => {
         this.props.createStream(formValues);
     }
 
     render(){
-        // console.log(this.props);
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-                    <Field name="title" component={this.renderInput} label="Enter Title" />
-                    <Field name="description" component={this.renderInput} label="Description" />
-                    <button className="ui button primary" text="submit">Submit</button>
-                </form>
+                <h3>Create a Stream</h3>
+               <StreamForm onSubmit={this.onSubmit} />
             </div>
         );
     }  
 }
 
-const validate = (formValues) => {
-    const errors={};
-
-    if(!formValues.title){
-        errors.title="There is no title"
-    }
-
-    if(!formValues.description){
-        errors.description = "Invalid description"
-    }
-
-    return errors;
-}
-
-
-// Way 1
-// export default connect() ( reduxForm({
-//     form:'Create Stream',
-//     validate: validate
-// })(StreamCreate));
-
-const formWrapped = reduxForm({
-    form: 'Create Stream',
-    validate:validate
-})(StreamCreate);
-
-export default connect(null, {createStream})(formWrapped);
+export default connect(null, {createStream})(StreamCreate);
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -15,7 +15,8 @@ class StreamList extends React.Component{
                 <div className="right floated content">
                     <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
                     {/* <button className="ui button primary">Edit</button> */}
-                    <button className="ui button negative">Delete</button>
+                    {/* <button className="ui red button">Delete</button> */}
+                    <Link to={`/streams/delete/${stream.id}`} className= "ui red button">Delete</Link>
                 </div>
             );   
         }
@@ -29,7 +30,7 @@ class StreamList extends React.Component{
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`}> {stream.title} </Link>
                         <div className="description">
                             {stream.description}
                         </div>
